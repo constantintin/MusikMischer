bbfcff77fa69bcbdd3cf51e62ee5fae655f5eb3d
Haddock <consti.loew@live.com>
2022-11-16 11:20:53 +0100

    Add like, unlike to Sorter

--- Musik/Views/TrackView.swift
+++ Musik/Views/TrackView.swift
@@ -13,6 +13,9 @@ struct TrackView: View {
     @Binding var opacity: Double
     @Binding var track: Track
     
+    @State private var liked = false
+    @State private var operating: Bool = false
+    
     @State private var didRequestImage = false
     @State private var image = Image(.spotifyAlbumPlaceholder)
 
@@ -31,6 +34,17 @@ struct TrackView: View {
                 .padding(.leading, 15)
                 .font(.system(size: 13))
             Spacer()
+            likedImage
+                .onTapGesture {
+                    if !self.operating {
+                        if liked {
+                            unlike()
+                        } else {
+                            like()
+                        }
+                    }
+                }
+                .padding(.trailing, 5)
         }
         // Ensure the hit box extends across the entire width of the frame.
         // See https://bit.ly/2HqNk4S
@@ -41,12 +55,63 @@ struct TrackView: View {
         .fixedSize(horizontal: false, vertical: true)
         .contentShape(Rectangle())
         .onAppear {
+            isLiked()
             loadImage()
         }
         .onChange(of: self.track, perform: { _ in
+            isLiked()
             self.didRequestImage = false
             loadImage()
         })
+    }
+    
+    /// heart either filled or empty
+    var likedImage: some View {
+        if liked {
+            return Image(systemName: "heart.fill")
+                .font(.title)
+                .scaleEffect(0.8)
+                .foregroundColor(.green)
+        } else {
+            return Image(systemName: "heart")
+                .font(.title)
+                .scaleEffect(0.8)
+                .foregroundColor(.green)
+        }
+    }
+    
+    /// save this track for user
+    func like() {
+        self.operating = true
+        self.liked.toggle()
+        if let uri = track.uri {
+            spotify.api.saveTracksForCurrentUser([uri])
+                .receive(on: RunLoop.main)
+                .sink(
+                    receiveCompletion: { _ in
+                        self.operating = false
+                    },
+                    receiveValue: { _ in }
+                )
+                .store(in: &cancellables)
+        }
+    }
+    
+    /// save this track for user
+    func unlike() {
+        self.operating = true
+        self.liked.toggle()
+        if let uri = track.uri {
+            spotify.api.removeSavedTracksForCurrentUser([uri])
+                .receive(on: RunLoop.main)
+                .sink(
+                    receiveCompletion: { _ in
+                        self.operating = false
+                    },
+                    receiveValue: { _ in }
+                )
+                .store(in: &cancellables)
+        }
     }
     
     /// The display name for the track. E.g., "Eclipse - Pink Floyd".
@@ -56,6 +121,23 @@ struct TrackView: View {
             displayName += " - \(artistName)"
         }
         return displayName
+    }
+    
+    /// check whether track is saved or not and store in self.liked
+    func isLiked() {
+        if let uri = track.uri {
+            spotify.api.currentUserSavedTracksContains([uri])
+                .receive(on: RunLoop.main)
+                .sink(
+                    receiveCompletion: { _ in },
+                    receiveValue: { bools in
+                        if let liked = bools.first {
+                            self.liked = liked
+                        }
+                    }
+                )
+                .store(in: &cancellables)
+        }
     }
     
     /// load album image
